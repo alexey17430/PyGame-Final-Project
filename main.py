e8c5a901cb66daf246eed13a7396ba636c40f703
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-01-07 22:58:13 +0500

    Начата работа над интерфейсом стартового окна.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import pygame_gui
 import random
 import sys
 import os
@@ -21,10 +22,7 @@ def load_image(name, colorkey=None):
     return image
 
 
-if __name__ == '__main__':
-    pygame.init()
-    size = w, h = 750, 750
-    screen = pygame.display.set_mode(size)
+def main():
     running = True
     clock = pygame.time.Clock()
     fps = 60
@@ -83,4 +81,89 @@ if __name__ == '__main__':
         all_sprites.update()
         clock.tick(fps)
         pygame.display.flip()
-pygame.quit()
+
+
+def start_window():
+    manager = pygame_gui.UIManager((750, 750))
+    clock = pygame.time.Clock()
+    run = True
+    flag_game_start_pushed = False
+
+    start_game_button = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect((275, 250), (200, 50)),
+        text='Старт',
+        manager=manager
+    )
+
+    results_table_button = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect((275, 350), (200, 50)),
+        text='Таблица результатов',
+        manager=manager
+    )
+
+    line_player_name = pygame_gui.elements.UITextEntryLine(
+        relative_rect=pygame.Rect((275, 450), (200, 50)),
+        manager=manager
+    )
+
+    while run:
+        time_delta = clock.tick(60) / 1000
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                confirmation_dialog = pygame_gui.windows.UIConfirmationDialog(
+                    rect=pygame.Rect((250, 200), (300, 200)),
+                    manager=manager,
+                    window_title='Подтверждение',
+                    action_long_desc='Вы уверены, что хотите выйти?',
+                    action_short_name='OK',
+                    blocking=True
+                )
+            if event.type == pygame.USEREVENT:
+                if event.user_type == pygame_gui.UI_CONFIRMATION_DIALOG_CONFIRMED:
+                    if event.ui_element == confirmation_dialog:
+                        run = False
+            if event.type == pygame.USEREVENT:
+                if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                    if event.ui_element == start_game_button:
+                        flag_game_start_pushed = True
+                        screen.fill((0, 0, 0))
+                        font = pygame.font.Font(None, 50)
+                        text = font.render("Игра начинается", True, (100, 255, 100))
+                        text_x = w // 2 - text.get_width() // 2
+                        text_y = h // 2 - text.get_height() // 2 - 200
+                        text_w = text.get_width()
+                        text_h = text.get_height()
+                        screen.blit(text, (text_x, text_y))
+                        pygame.draw.rect(screen, (0, 255, 0), (text_x - 10, text_y - 10,
+                                                               text_w + 20, text_h + 20), 1)
+
+                if event.user_type == pygame_gui.UI_BUTTON_ON_HOVERED and not flag_game_start_pushed:
+                    if event.ui_element == start_game_button:
+                        screen.fill((0, 0, 0))
+                        font = pygame.font.Font(None, 50)
+                        text = font.render("Игра готовится к запуску...", True, (100, 255, 100))
+                        text_x = w // 2 - text.get_width() // 2
+                        text_y = h // 2 - text.get_height() // 2 - 200
+                        text_w = text.get_width()
+                        text_h = text.get_height()
+                        screen.blit(text, (text_x, text_y))
+                        pygame.draw.rect(screen, (0, 255, 0), (text_x - 10, text_y - 10,
+                                                               text_w + 20, text_h + 20), 1)
+
+                if event.user_type == pygame_gui.UI_BUTTON_ON_UNHOVERED and not flag_game_start_pushed:
+                    if event.ui_element == start_game_button:
+                        screen.fill((0, 0, 0))
+
+            manager.process_events(event)
+        manager.update(time_delta)
+        manager.draw_ui(screen)
+        pygame.display.update()
+
+
+if __name__ == '__main__':
+    pygame.init()
+    size = w, h = 750, 750
+    screen = pygame.display.set_mode(size)
+    start_window()
+    #main()
+    pygame.quit()
